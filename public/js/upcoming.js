843be5368f2df4c68e894acfbcda5bc84c7ba553
EslamTeleb1 <et1136@fayoum.edu.eg>
2021-01-11 14:01:30 +0200

    recent news links

--- public/js/upcoming.js
+++ public/js/upcoming.js
@@ -933,7 +933,7 @@ document.querySelector('.trn-card__header').insertAdjacentHTML('beforeend', nwHt
     {
        var border=result.items[i].rarity==='uncommon'?'handmade':result.items[i].rarity;
 
- var html=`<a class="fortnite-db-item fortnite-db-item--large-text fortnite-db-item--%rarity% " style="grid-column: span 1;" href="#" id="%id%"><img loading="lazy" class="fortnite-db-item__image" src="%img%" alt="fortnite shop item" /><div class="fortnite-db-item__details"><div class="fortnite-db-item__name" >%name%</div><div class="fortnite-db-item__price"><img loading="lazy" src="https://cdn.thetrackernetwork.com/cdn/trackernetwork/3C7Avbucks.png" alt="2000 v-bucks" /> <span>%price%</span></div></div><div class="fortnite-db-item__rating"><i class="ion ion-star"></i> %star%</div></a>`;
+ var html=`<a class="fortnite-db-item fortnite-db-item--large-text fortnite-db-item--%rarity% " style="grid-column: span 1;" href="/details?id=%id%" id="%id%"><img loading="lazy" class="fortnite-db-item__image" src="%img%" alt="fortnite shop item" /><div class="fortnite-db-item__details"><div class="fortnite-db-item__name" >%name%</div><div class="fortnite-db-item__price"><img loading="lazy" src="https://cdn.thetrackernetwork.com/cdn/trackernetwork/3C7Avbucks.png" alt="2000 v-bucks" /> <span>%price%</span></div></div><div class="fortnite-db-item__rating"><i class="ion ion-star"></i> %star%</div></a>`;
 
   nwHtml=html.replace('%id%', result.items[i].id);
   // src of the image
